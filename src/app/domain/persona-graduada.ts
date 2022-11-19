08cd05a68a4a7991c67a90f0d449a2fbec141527
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-11-19 14:39:33 -0600

    persona-graduada

---
+++ src/app/domain/persona-graduada.ts
@@ -0,0 +1,17 @@
+import { PlanEstudio } from "./plan-estudio";
+import { Recinto } from "./recinto";
+
+export class PersonaGraduada {
+    constructor(
+        public id?: String,
+        public nombre?: String,
+        public apellidos?: String,
+        public anoGraduacion?: number,
+        public numCarne?: String,
+        public promedioPonderado?: Number,
+        public telefono?: String,
+        public correo?: String,
+        public carreras?: Array<PlanEstudio>,
+        public recinto?: Recinto
+    ) { }
+}
