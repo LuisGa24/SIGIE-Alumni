c9578396312e8a6e284c5570d1f402dca4a8b01b
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-11-19 14:39:22 -0600

    consulta-mejora

---
+++ src/app/domain/consulta-mejora.spec.ts
@@ -0,0 +1,7 @@
+import { ConsultaMejora } from './consulta-mejora';
+
+describe('ConsultaMejora', () => {
+  it('should create an instance', () => {
+    expect(new ConsultaMejora()).toBeTruthy();
+  });
+});
