ada3077e05cd44849faecb7a28fa8e151e887e84
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-11-19 14:40:16 -0600

    respuesta-mejora

---
+++ src/app/domain/respuesta-mejora.spec.ts
@@ -0,0 +1,7 @@
+import { RespuestaMejora } from './respuesta-mejora';
+
+describe('RespuestaMejora', () => {
+  it('should create an instance', () => {
+    expect(new RespuestaMejora()).toBeTruthy();
+  });
+});
---
+++ src/app/domain/respuesta-mejora.ts
@@ -0,0 +1,13 @@
+import { CategoriaConsulta } from "./categoria-consulta";
+import { ConsultaMejora } from "./consulta-mejora";
+import { PersonaGraduada } from "./persona-graduada";
+
+export class RespuestaMejora {
+    constructor(
+        public id?: String,
+        public propuesta?: String,
+        public categoriaConsulta?: CategoriaConsulta,
+        public consultaMejora?: ConsultaMejora,
+        public autor?: PersonaGraduada
+    ) { }
+}
